c15526982164707401064dc69c3e0cfae96c9b54
emengjzs <emengjzs@live.com>
2016-12-11 21:20:03 +0800

    Add and start httpServer in index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-(async function() {
-    console.log("hello world");
-})();
+import express from "express";
+
+import app from "./lib/app";
+import httpServer from "./lib/http/server";
+
+app.attach(httpServer);
+
+const port = 8000;
+httpServer.listen(() => {
+    console.log(`The server is now listening on ${port}`);
+});
+
+
+
+
